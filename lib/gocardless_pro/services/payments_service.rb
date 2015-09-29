9c7e37e0b1d8d7c040f41dde67ef3f62d8187c94
Crankshaft Robot <robot+crankshaft@gocardless.com>
2015-09-29 14:18:29 +0000

    Crankshaft[BU0000021VY1S4] Client - Ruby @ 2015-09-29 14:18:28 +0000

--- lib/gocardless_pro/services/payments_service.rb
+++ lib/gocardless_pro/services/payments_service.rb
@@ -15,7 +15,8 @@ module GoCardlessPro
       # This
       # fails with a `mandate_is_inactive` error if the linked
       # [mandate](#core-endpoints-mandates) is cancelled or has failed. Payments can
-      # be created against `pending_submission`  and `submitted` mandates.
+      # be created against `pending_submission` and `submitted` as well as `active`
+      # mandates.
       # Example URL: /payments
       # @param options [Hash] parameters as a hash, under a params key.
       def create(options = {})
