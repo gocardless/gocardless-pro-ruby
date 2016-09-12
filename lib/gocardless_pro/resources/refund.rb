15c65fb051d185cf2decfb7cf507924100c61533
Crankshaft Robot <robot+crankshaft@gocardless.com>
2016-09-12 08:58:44 +0000

    Crankshaft[BU000002ZMAJNJ] Client - Ruby @ 2016-09-12 08:58:43 +0000

--- lib/gocardless_pro/resources/refund.rb
+++ lib/gocardless_pro/resources/refund.rb
@@ -18,10 +18,6 @@ module GoCardlessPro
     # GoCardless will notify you
     # via a [webhook](#webhooks) whenever a refund is created, and will update the
     # `amount_refunded` property of the payment.
-    #
-    # _Note:_ A payment that
-    # has been (partially) refunded can still receive a late failure or chargeback
-    # from the banks.
     # Represents an instance of a refund resource returned from the API
     class Refund
       attr_reader :amount
