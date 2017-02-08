f96fef0bf9f9fb907481b4ad584ef06c51340776
Crankshaft Robot <robot+crankshaft@gocardless.com>
2017-02-08 14:44:29 +0000

    Crankshaft[BU000003D5BHSN] Client - Ruby @ 2017-02-08 14:44:29 +0000

--- lib/gocardless_pro/resources/customer_bank_account.rb
+++ lib/gocardless_pro/resources/customer_bank_account.rb
@@ -20,7 +20,7 @@ module GoCardlessPro
     # accounts must be unique, and so you will encounter a `bank_account_exists`
     # error if you try to create a duplicate bank account. You may wish to handle
     # this by updating the existing record instead, the ID of which will be
-    # provided as links[customer_bank_account] in the error response.
+    # provided as `links[customer_bank_account]` in the error response.
     # Represents an instance of a customer_bank_account resource returned from the API
     class CustomerBankAccount
       attr_reader :account_holder_name
