3ff05cd9ba423fc082c9a6f5a46ceffdf4040897
Crankshaft Robot <robot+crankshaft@gocardless.com>
2018-04-11 11:19:39 +0000

    Add default_aud_payout_account link to Creditor

--- lib/gocardless_pro/resources/creditor.rb
+++ lib/gocardless_pro/resources/creditor.rb
@@ -78,6 +78,10 @@ module GoCardlessPro
           @links = links || {}
         end
 
+        def default_aud_payout_account
+          @links['default_aud_payout_account']
+        end
+
         def default_dkk_payout_account
           @links['default_dkk_payout_account']
         end
