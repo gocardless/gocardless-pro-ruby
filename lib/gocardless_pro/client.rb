d3e7b7f6e2162b19becef357faa1c7982335239e
Crankshaft Robot <robot+crankshaft@gocardless.com>
2018-04-26 09:52:39 +0000

    Bump version to v2.8.0

--- lib/gocardless_pro/client.rb
+++ lib/gocardless_pro/client.rb
@@ -133,7 +133,7 @@ module GoCardlessPro
           'User-Agent' => user_agent.to_s,
           'Content-Type' => 'application/json',
           'GoCardless-Client-Library' => 'gocardless-pro-ruby',
-          'GoCardless-Client-Version' => '2.7.0',
+          'GoCardless-Client-Version' => '2.8.0',
         },
       }
     end
