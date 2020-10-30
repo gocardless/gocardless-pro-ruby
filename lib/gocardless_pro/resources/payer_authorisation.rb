747ed268b2905bb3cbe4da8aee6fa5bd0f78ab44
Robot <robot@gocardless.com>
2020-10-30 10:37:44 +0000

    Added a new resource called payer_authorisatons
    This is our new API dedicated to building custom payment pages.
    It encapsulates the creation of customer, bank account and mandate under this single resource.

---
+++ lib/gocardless_pro/resources/payer_authorisation.rb
@@ -0,0 +1,119 @@
+# encoding: utf-8
+
+#
+# This client is automatically generated from a template and JSON schema definition.
+# See https://github.com/gocardless/gocardless-pro-ruby#contributing before editing.
+#
+
+require 'uri'
+
+module GoCardlessPro
+  # A module containing classes for each of the resources in the GC Api
+  module Resources
+    # Represents an instance of a payer_authorisation resource returned from the API
+
+    # Payer Authorisation resource acts as a wrapper for creating customer, bank
+    # account and mandate details in a single request.
+    # PayerAuthorisation API enables the integrators to build their own custom
+    # payment pages.
+    #
+    # The process to use the Payer Authorisation API is as follows:
+    #
+    #   1. Create a Payer Authorisation, either empty or with already available
+    # information
+    #   2. Update the authorisation with additional information or fix any
+    # mistakes
+    #   3. Submit the authorisation, after the payer has reviewed their
+    # information
+    #   4. [coming soon] Redirect the payer to the verification mechanisms from
+    # the response of the Submit request (this will be introduced as a
+    # non-breaking change)
+    #   5. Confirm the authorisation to indicate that the resources can be
+    # created
+    #
+    # After the Payer Authorisation is confirmed, resources will eventually be
+    # created as it's an asynchronous process.
+    #
+    # To retrieve the status and ID of the linked resources you can do one of
+    # the following:
+    # <ol>
+    #   <li> Listen to <code>  payer_authorisation_completed </code>  <a
+    # href="#appendix-webhooks"> webhook</a> (recommended)</li>
+    #   <li> Poll the GET <a
+    # href="#payer-authorisations-get-a-single-payer-authorisation">
+    # endpoint</a></li>
+    #   <li> Poll the GET events API
+    # <code>https://api.gocardless.com/events?payer_authorisation={id}&action=completed</code>
+    # </li>
+    # </ol>
+    #
+    # <p class="notice">
+    #   Note that the `create` and `update` endpoints behave differently than
+    #   other existing `create` and `update` endpoints. The Payer Authorisation
+    # is still saved if incomplete data is provided.
+    #   We return the list of incomplete data in the `incomplete_fields` along
+    # with the resources in the body of the response.
+    #   <br><br>
+    #   The API is designed to be flexible and allows you to collect information
+    # in multiple steps without storing any sensitive data in the browser or in
+    # your servers.
+    # </p>
+    class PayerAuthorisation
+      attr_reader :bank_account
+      attr_reader :created_at
+      attr_reader :customer
+      attr_reader :id
+      attr_reader :incomplete_fields
+      attr_reader :mandate
+      attr_reader :status
+
+      # Initialize a payer_authorisation resource instance
+      # @param object [Hash] an object returned from the API
+      def initialize(object, response = nil)
+        @object = object
+
+        @bank_account = object['bank_account']
+        @created_at = object['created_at']
+        @customer = object['customer']
+        @id = object['id']
+        @incomplete_fields = object['incomplete_fields']
+        @links = object['links']
+        @mandate = object['mandate']
+        @status = object['status']
+        @response = response
+      end
+
+      def api_response
+        ApiResponse.new(@response)
+      end
+
+      # Return the links that the resource has
+      def links
+        @payer_authorisation_links ||= Links.new(@links)
+      end
+
+      # Provides the payer_authorisation resource as a hash of all its readable attributes
+      def to_h
+        @object
+      end
+
+      class Links
+        def initialize(links)
+          @links = links || {}
+        end
+
+        def bank_account
+          @links['bank_account']
+        end
+
+        def customer
+          @links['customer']
+        end
+
+        def mandate
+          @links['mandate']
+        end
+      end
+    end
+  end
+end
---
+++ spec/resources/payer_authorisation_spec.rb
@@ -0,0 +1,418 @@
+require 'spec_helper'
+
+describe GoCardlessPro::Resources::PayerAuthorisation do
+  let(:client) do
+    GoCardlessPro::Client.new(
+      access_token: 'SECRET_TOKEN'
+    )
+  end
+
+  let(:response_headers) { { 'Content-Type' => 'application/json' } }
+
+  describe '#get' do
+    let(:id) { 'ID123' }
+
+    subject(:get_response) { client.payer_authorisations.get(id) }
+
+    context 'passing in a custom header' do
+      let!(:stub) do
+        stub_url = '/payer_authorisations/:identity'.gsub(':identity', id)
+        stub_request(:get, /.*api.gocardless.com#{stub_url}/).
+          with(headers: { 'Foo' => 'Bar' }).
+          to_return(
+            body: {
+              'payer_authorisations' => {
+
+                'bank_account' => 'bank_account-input',
+                'created_at' => 'created_at-input',
+                'customer' => 'customer-input',
+                'id' => 'id-input',
+                'incomplete_fields' => 'incomplete_fields-input',
+                'links' => 'links-input',
+                'mandate' => 'mandate-input',
+                'status' => 'status-input',
+              },
+            }.to_json,
+            headers: response_headers
+          )
+      end
+
+      subject(:get_response) do
+        client.payer_authorisations.get(id, headers: {
+                                          'Foo' => 'Bar',
+                                        })
+      end
+
+      it 'includes the header' do
+        get_response
+        expect(stub).to have_been_requested
+      end
+    end
+
+    context 'when there is a payer_authorisation to return' do
+      before do
+        stub_url = '/payer_authorisations/:identity'.gsub(':identity', id)
+        stub_request(:get, /.*api.gocardless.com#{stub_url}/).to_return(
+          body: {
+            'payer_authorisations' => {
+
+              'bank_account' => 'bank_account-input',
+              'created_at' => 'created_at-input',
+              'customer' => 'customer-input',
+              'id' => 'id-input',
+              'incomplete_fields' => 'incomplete_fields-input',
+              'links' => 'links-input',
+              'mandate' => 'mandate-input',
+              'status' => 'status-input',
+            },
+          }.to_json,
+          headers: response_headers
+        )
+      end
+
+      it 'wraps the response in a resource' do
+        expect(get_response).to be_a(GoCardlessPro::Resources::PayerAuthorisation)
+      end
+    end
+
+    context 'when nothing is returned' do
+      before do
+        stub_url = '/payer_authorisations/:identity'.gsub(':identity', id)
+        stub_request(:get, /.*api.gocardless.com#{stub_url}/).to_return(
+          body: '',
+          headers: response_headers
+        )
+      end
+
+      it 'returns nil' do
+        expect(get_response).to be_nil
+      end
+    end
+
+    context "when an ID is specified which can't be included in a valid URI" do
+      let(:id) { '`' }
+
+      it "doesn't raise an error" do
+        expect { get_response }.to_not raise_error(/bad URI/)
+      end
+    end
+  end
+
+  describe '#create' do
+    subject(:post_create_response) { client.payer_authorisations.create(params: new_resource) }
+    context 'with a valid request' do
+      let(:new_resource) do
+        {
+
+          'bank_account' => 'bank_account-input',
+          'created_at' => 'created_at-input',
+          'customer' => 'customer-input',
+          'id' => 'id-input',
+          'incomplete_fields' => 'incomplete_fields-input',
+          'links' => 'links-input',
+          'mandate' => 'mandate-input',
+          'status' => 'status-input',
+        }
+      end
+
+      before do
+        stub_request(:post, %r{.*api.gocardless.com/payer_authorisations}).
+          with(
+            body: {
+              'payer_authorisations' => {
+
+                'bank_account' => 'bank_account-input',
+                'created_at' => 'created_at-input',
+                'customer' => 'customer-input',
+                'id' => 'id-input',
+                'incomplete_fields' => 'incomplete_fields-input',
+                'links' => 'links-input',
+                'mandate' => 'mandate-input',
+                'status' => 'status-input',
+              },
+            }
+          ).
+          to_return(
+            body: {
+              'payer_authorisations' =>
+
+                {
+
+                  'bank_account' => 'bank_account-input',
+                  'created_at' => 'created_at-input',
+                  'customer' => 'customer-input',
+                  'id' => 'id-input',
+                  'incomplete_fields' => 'incomplete_fields-input',
+                  'links' => 'links-input',
+                  'mandate' => 'mandate-input',
+                  'status' => 'status-input',
+                },
+
+            }.to_json,
+            headers: response_headers
+          )
+      end
+
+      it 'creates and returns the resource' do
+        expect(post_create_response).to be_a(GoCardlessPro::Resources::PayerAuthorisation)
+      end
+    end
+
+    context 'with a request that returns a validation error' do
+      let(:new_resource) { {} }
+
+      before do
+        stub_request(:post, %r{.*api.gocardless.com/payer_authorisations}).to_return(
+          body: {
+            error: {
+              type: 'validation_failed',
+              code: 422,
+              errors: [
+                { message: 'test error message', field: 'test_field' },
+              ],
+            },
+          }.to_json,
+          headers: response_headers,
+          status: 422
+        )
+      end
+
+      it 'throws the correct error' do
+        expect { post_create_response }.to raise_error(GoCardlessPro::ValidationError)
+      end
+    end
+
+    context 'with a request that returns an idempotent creation conflict error' do
+      let(:id) { 'ID123' }
+
+      let(:new_resource) do
+        {
+
+          'bank_account' => 'bank_account-input',
+          'created_at' => 'created_at-input',
+          'customer' => 'customer-input',
+          'id' => 'id-input',
+          'incomplete_fields' => 'incomplete_fields-input',
+          'links' => 'links-input',
+          'mandate' => 'mandate-input',
+          'status' => 'status-input',
+        }
+      end
+
+      let!(:post_stub) do
+        stub_request(:post, %r{.*api.gocardless.com/payer_authorisations}).to_return(
+          body: {
+            error: {
+              type: 'invalid_state',
+              code: 409,
+              errors: [
+                {
+                  message: 'A resource has already been created with this idempotency key',
+                  reason: 'idempotent_creation_conflict',
+                  links: {
+                    conflicting_resource_id: id,
+                  },
+                },
+              ],
+            },
+          }.to_json,
+          headers: response_headers,
+          status: 409
+        )
+      end
+
+      let!(:get_stub) do
+        stub_url = "/payer_authorisations/#{id}"
+        stub_request(:get, /.*api.gocardless.com#{stub_url}/).
+          to_return(
+            body: {
+              'payer_authorisations' => {
+
+                'bank_account' => 'bank_account-input',
+                'created_at' => 'created_at-input',
+                'customer' => 'customer-input',
+                'id' => 'id-input',
+                'incomplete_fields' => 'incomplete_fields-input',
+                'links' => 'links-input',
+                'mandate' => 'mandate-input',
+                'status' => 'status-input',
+              },
+            }.to_json,
+            headers: response_headers
+          )
+      end
+
+      it 'fetches the already-created resource' do
+        post_create_response
+        expect(post_stub).to have_been_requested
+        expect(get_stub).to have_been_requested
+      end
+    end
+  end
+
+  describe '#update' do
+    subject(:put_update_response) { client.payer_authorisations.update(id, params: update_params) }
+    let(:id) { 'ABC123' }
+
+    context 'with a valid request' do
+      let(:update_params) { { 'hello' => 'world' } }
+
+      let!(:stub) do
+        stub_url = '/payer_authorisations/:identity'.gsub(':identity', id)
+        stub_request(:put, /.*api.gocardless.com#{stub_url}/).to_return(
+          body: {
+            'payer_authorisations' => {
+
+              'bank_account' => 'bank_account-input',
+              'created_at' => 'created_at-input',
+              'customer' => 'customer-input',
+              'id' => 'id-input',
+              'incomplete_fields' => 'incomplete_fields-input',
+              'links' => 'links-input',
+              'mandate' => 'mandate-input',
+              'status' => 'status-input',
+            },
+          }.to_json,
+          headers: response_headers
+        )
+      end
+
+      it 'updates and returns the resource' do
+        expect(put_update_response).to be_a(GoCardlessPro::Resources::PayerAuthorisation)
+        expect(stub).to have_been_requested
+      end
+    end
+  end
+
+  describe '#submit' do
+    subject(:post_response) { client.payer_authorisations.submit(resource_id) }
+
+    let(:resource_id) { 'ABC123' }
+
+    let!(:stub) do
+      # /payer_authorisations/%v/actions/submit
+      stub_url = '/payer_authorisations/:identity/actions/submit'.gsub(':identity', resource_id)
+      stub_request(:post, /.*api.gocardless.com#{stub_url}/).to_return(
+        body: {
+          'payer_authorisations' => {
+
+            'bank_account' => 'bank_account-input',
+            'created_at' => 'created_at-input',
+            'customer' => 'customer-input',
+            'id' => 'id-input',
+            'incomplete_fields' => 'incomplete_fields-input',
+            'links' => 'links-input',
+            'mandate' => 'mandate-input',
+            'status' => 'status-input',
+          },
+        }.to_json,
+        headers: response_headers
+      )
+    end
+
+    it 'wraps the response and calls the right endpoint' do
+      expect(post_response).to be_a(GoCardlessPro::Resources::PayerAuthorisation)
+
+      expect(stub).to have_been_requested
+    end
+
+    context 'when the request needs a body and custom header' do
+      let(:body) { { foo: 'bar' } }
+      let(:headers) { { 'Foo' => 'Bar' } }
+      subject(:post_response) { client.payer_authorisations.submit(resource_id, body, headers) }
+
+      let(:resource_id) { 'ABC123' }
+
+      let!(:stub) do
+        # /payer_authorisations/%v/actions/submit
+        stub_url = '/payer_authorisations/:identity/actions/submit'.gsub(':identity', resource_id)
+        stub_request(:post, /.*api.gocardless.com#{stub_url}/).
+          with(
+            body: { foo: 'bar' },
+            headers: { 'Foo' => 'Bar' }
+          ).to_return(
+            body: {
+              'payer_authorisations' => {
+
+                'bank_account' => 'bank_account-input',
+                'created_at' => 'created_at-input',
+                'customer' => 'customer-input',
+                'id' => 'id-input',
+                'incomplete_fields' => 'incomplete_fields-input',
+                'links' => 'links-input',
+                'mandate' => 'mandate-input',
+                'status' => 'status-input',
+              },
+            }.to_json,
+            headers: response_headers
+          )
+      end
+    end
+  end
+
+  describe '#confirm' do
+    subject(:post_response) { client.payer_authorisations.confirm(resource_id) }
+
+    let(:resource_id) { 'ABC123' }
+
+    let!(:stub) do
+      # /payer_authorisations/%v/actions/confirm
+      stub_url = '/payer_authorisations/:identity/actions/confirm'.gsub(':identity', resource_id)
+      stub_request(:post, /.*api.gocardless.com#{stub_url}/).to_return(
+        body: {
+          'payer_authorisations' => {
+
+            'bank_account' => 'bank_account-input',
+            'created_at' => 'created_at-input',
+            'customer' => 'customer-input',
+            'id' => 'id-input',
+            'incomplete_fields' => 'incomplete_fields-input',
+            'links' => 'links-input',
+            'mandate' => 'mandate-input',
+            'status' => 'status-input',
+          },
+        }.to_json,
+        headers: response_headers
+      )
+    end
+
+    it 'wraps the response and calls the right endpoint' do
+      expect(post_response).to be_a(GoCardlessPro::Resources::PayerAuthorisation)
+
+      expect(stub).to have_been_requested
+    end
+
+    context 'when the request needs a body and custom header' do
+      let(:body) { { foo: 'bar' } }
+      let(:headers) { { 'Foo' => 'Bar' } }
+      subject(:post_response) { client.payer_authorisations.confirm(resource_id, body, headers) }
+
+      let(:resource_id) { 'ABC123' }
+
+      let!(:stub) do
+        # /payer_authorisations/%v/actions/confirm
+        stub_url = '/payer_authorisations/:identity/actions/confirm'.gsub(':identity', resource_id)
+        stub_request(:post, /.*api.gocardless.com#{stub_url}/).
+          with(
+            body: { foo: 'bar' },
+            headers: { 'Foo' => 'Bar' }
+          ).to_return(
+            body: {
+              'payer_authorisations' => {
+
+                'bank_account' => 'bank_account-input',
+                'created_at' => 'created_at-input',
+                'customer' => 'customer-input',
+                'id' => 'id-input',
+                'incomplete_fields' => 'incomplete_fields-input',
+                'links' => 'links-input',
+                'mandate' => 'mandate-input',
+                'status' => 'status-input',
+              },
+            }.to_json,
+            headers: response_headers
+          )
+      end
+    end
+  end
+end
