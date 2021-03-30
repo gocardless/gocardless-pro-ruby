4fc76321eb8e4d10d28fefcf4b68603aafacd735
Robot <robot@gocardless.com>
2021-03-30 11:11:04 +0000

    Changes generated by e055e38516d13de24e4934b300dd8c9104f97bbb

--- lib/gocardless_pro/services/scenario_simulators_service.rb
+++ lib/gocardless_pro/services/scenario_simulators_service.rb
@@ -93,6 +93,10 @@ module GoCardlessPro
       # in either the `pending_submission`, `submitted`, or `paid` state.</li>
       # <li>`payout_bounced`: Transitions a payout to `bounced`. It must start
       # in the `paid` state.</li>
+      # <li>`payout_create`: Creates a payout containing payments in
+      # `confirmed`, `failed` & `charged_back` states; refunds in `submitted` &
+      # `bounced`; and all related fees. Can only be used with a positive total
+      # payout balance and when some eligible items exist.</li>
       # </ul>
       # @param options [Hash] parameters as a hash, under a params key.
       def run(identity, options = {})
