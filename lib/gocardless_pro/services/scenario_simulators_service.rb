d3948a8fcaa2facb1461dc7f96aea98511673739
Robot <robot@gocardless.com>
2021-03-31 10:37:18 +0000

    Changes generated by 7074daf88fcfec93e6778e07e083e98b66702a37

--- lib/gocardless_pro/services/scenario_simulators_service.rb
+++ lib/gocardless_pro/services/scenario_simulators_service.rb
@@ -64,6 +64,13 @@ module GoCardlessPro
       # start in the `pending_submission` state. Not compatible with ACH, BECS,
       # BECS_NZ and SEPA mandates, which are submitted and activated with their
       # first payment.</li>
+      # <li>`mandate_customer_approval_granted`: Transitions a mandate through
+      # to `pending_submission`. It must start in the
+      # `pending_customer_approval` state. Compatible only with Bacs and SEPA
+      # mandates, which support customer signatures on the mandate. All payments
+      # associated with the mandate will be transitioned to
+      # `pending_submission`. All subscriptions associated with the mandate will
+      # become `active`.</li>
       # <li>`mandate_failed`: Transitions a mandate through to `failed`, having
       # been submitted to the banks but found to be invalid (for example due to
       # invalid bank details). It must start in the `pending_submission` or
