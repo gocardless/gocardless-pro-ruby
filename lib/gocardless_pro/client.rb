8537029b1f5cd8477db1f66a7b389f476a275100
Robot <robot@gocardless.com>
2021-04-13 14:17:20 +0000

    Changes generated by 210d23e132c9d8786e2f8792bcb1f02c9d82a3ff

--- lib/gocardless_pro/client.rb
+++ lib/gocardless_pro/client.rb
@@ -188,7 +188,7 @@ module GoCardlessPro
           'User-Agent' => user_agent.to_s,
           'Content-Type' => 'application/json',
           'GoCardless-Client-Library' => 'gocardless-pro-ruby',
-          'GoCardless-Client-Version' => '2.26.0',
+          'GoCardless-Client-Version' => '2.27.0',
         },
       }
     end
