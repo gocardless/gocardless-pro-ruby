a0252a6606a58bc33afb005b7d9a3337bd72205f
Robot <robot@gocardless.com>
2021-04-14 13:30:21 +0000

    Changes generated by 61388c3a5f937de38c98a4703f68e8ed7b3c4ad1

--- lib/gocardless_pro/services/scenario_simulators_service.rb
+++ lib/gocardless_pro/services/scenario_simulators_service.rb
@@ -120,17 +120,17 @@ module GoCardlessPro
       # it, and moves the associated payment to `failed`. The billing request
       # must be in the `pending` state, with all actions completed except for
       # `bank_authorisation`. Only billing requests with a `payment_request` are
-      # supported.<li>
+      # supported.</li>
       # <li>`billing_request_fulfilled_and_payment_failed`: Authorises the
       # billing request, fulfils it, and moves the associated payment to
       # `failed`. The billing request must be in the `pending` state, with all
       # actions completed except for `bank_authorisation`. Only billing requests
-      # with a `payment_request` are supported.<li>
+      # with a `payment_request` are supported.</li>
       # <li>`billing_request_fulfilled_and_payment_paid_out`: Authorises the
       # billing request, fulfils it, and moves the associated payment to
       # `paid_out`. The billing request must be in the `pending` state, with all
       # actions completed except for `bank_authorisation`. Only billing requests
-      # with a `payment_request` are supported.<li>
+      # with a `payment_request` are supported.</li>
       # </ul>
       # @param options [Hash] parameters as a hash, under a params key.
       def run(identity, options = {})
