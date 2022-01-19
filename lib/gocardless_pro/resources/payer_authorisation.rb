eeb7930d87a939284f0b64dc0c669208e003c4bf
Robot <robot@gocardless.com>
2022-01-19 09:46:22 +0000

    Changes generated by 21c255a1cdf21ae275429928dfa8e1027c8f94b3

--- lib/gocardless_pro/resources/payer_authorisation.rb
+++ lib/gocardless_pro/resources/payer_authorisation.rb
@@ -13,12 +13,12 @@ module GoCardlessPro
     # Represents an instance of a payer_authorisation resource returned from the API
 
     # <p class="restricted-notice">
-    #   Payer Authorisations is deprecated in favour of
+    #   Don't use Payer Authorisations for new integrations.
+    #   It is deprecated in favour of
     #   <a
     # href="https://developer.gocardless.com/getting-started/billing-requests/overview/">
-    #   Billing Requests</a>. Please consider using Billing Requests to build
-    # any
-    #   future integrations.
+    #   Billing Requests</a>. Use Billing Requests to build any future
+    # integrations.
     # </p>
     #
     # Payer Authorisation resource acts as a wrapper for creating customer, bank
