3ae00b90b11bdac77d13e4bd5dc9017da56da22e
Robot <robot@gocardless.com>
2023-02-13 17:13:28 +0000

    Changes from gocardless/gocardless-pro-ruby-template@bf5bd53ce3a55dd61cfff720b0bcb3a0b4c809b3

--- lib/gocardless_pro/services/verification_details_service.rb
+++ lib/gocardless_pro/services/verification_details_service.rb
@@ -10,6 +10,25 @@ module GoCardlessPro
   module Services
     # Service for making requests to the VerificationDetail endpoints
     class VerificationDetailsService < BaseService
+      # Creates a new verification detail
+      # Example URL: /verification_details
+      # @param options [Hash] parameters as a hash, under a params key.
+      def create(options = {})
+        path = '/verification_details'
+
+        params = options.delete(:params) || {}
+        options[:params] = {}
+        options[:params][envelope_key] = params
+
+        options[:retry_failures] = true
+
+        response = make_request(:post, path, options)
+
+        return if response.body.nil?
+
+        Resources::VerificationDetail.new(unenvelope_body(response.body), response)
+      end
+
       # Returns a list of verification details belonging to a creditor.
       # Example URL: /verification_details
       # @param options [Hash] parameters as a hash, under a params key.
@@ -36,25 +55,6 @@ module GoCardlessPro
           service: self,
           options: options
         ).enumerator
-      end
-
-      # Creates a new verification detail
-      # Example URL: /verification_details
-      # @param options [Hash] parameters as a hash, under a params key.
-      def create(options = {})
-        path = '/verification_details'
-
-        params = options.delete(:params) || {}
-        options[:params] = {}
-        options[:params][envelope_key] = params
-
-        options[:retry_failures] = true
-
-        response = make_request(:post, path, options)
-
-        return if response.body.nil?
-
-        Resources::VerificationDetail.new(unenvelope_body(response.body), response)
       end
 
       private
--- spec/resources/verification_detail_spec.rb
+++ spec/resources/verification_detail_spec.rb
@@ -9,128 +9,6 @@ describe GoCardlessPro::Resources::VerificationDetail do
 
   let(:response_headers) { { 'Content-Type' => 'application/json' } }
 
-  describe '#list' do
-    describe 'with no filters' do
-      subject(:get_list_response) { client.verification_details.list }
-
-      before do
-        stub_request(:get, %r{.*api.gocardless.com/verification_details}).to_return(
-          body: {
-            'verification_details' => [{
-
-              'address_line1' => 'address_line1-input',
-              'address_line2' => 'address_line2-input',
-              'address_line3' => 'address_line3-input',
-              'city' => 'city-input',
-              'company_number' => 'company_number-input',
-              'description' => 'description-input',
-              'directors' => 'directors-input',
-              'links' => 'links-input',
-              'name' => 'name-input',
-              'postal_code' => 'postal_code-input',
-            }],
-            meta: {
-              cursors: {
-                before: nil,
-                after: 'ABC123',
-              },
-            },
-          }.to_json,
-          headers: response_headers
-        )
-      end
-
-      it 'wraps each item in the resource class' do
-        expect(get_list_response.records.map do |x|
-                 x.class
-               end.uniq.first).to eq(GoCardlessPro::Resources::VerificationDetail)
-
-        expect(get_list_response.records.first.address_line1).to eq('address_line1-input')
-
-        expect(get_list_response.records.first.address_line2).to eq('address_line2-input')
-
-        expect(get_list_response.records.first.address_line3).to eq('address_line3-input')
-
-        expect(get_list_response.records.first.city).to eq('city-input')
-
-        expect(get_list_response.records.first.company_number).to eq('company_number-input')
-
-        expect(get_list_response.records.first.description).to eq('description-input')
-
-        expect(get_list_response.records.first.directors).to eq('directors-input')
-
-        expect(get_list_response.records.first.name).to eq('name-input')
-
-        expect(get_list_response.records.first.postal_code).to eq('postal_code-input')
-      end
-
-      it 'exposes the cursors for before and after' do
-        expect(get_list_response.before).to eq(nil)
-        expect(get_list_response.after).to eq('ABC123')
-      end
-
-      specify { expect(get_list_response.api_response.headers).to eql('content-type' => 'application/json') }
-    end
-  end
-
-  describe '#all' do
-    let!(:first_response_stub) do
-      stub_request(:get, %r{.*api.gocardless.com/verification_details$}).to_return(
-        body: {
-          'verification_details' => [{
-
-            'address_line1' => 'address_line1-input',
-            'address_line2' => 'address_line2-input',
-            'address_line3' => 'address_line3-input',
-            'city' => 'city-input',
-            'company_number' => 'company_number-input',
-            'description' => 'description-input',
-            'directors' => 'directors-input',
-            'links' => 'links-input',
-            'name' => 'name-input',
-            'postal_code' => 'postal_code-input',
-          }],
-          meta: {
-            cursors: { after: 'AB345' },
-            limit: 1,
-          },
-        }.to_json,
-        headers: response_headers
-      )
-    end
-
-    let!(:second_response_stub) do
-      stub_request(:get, %r{.*api.gocardless.com/verification_details\?after=AB345}).to_return(
-        body: {
-          'verification_details' => [{
-
-            'address_line1' => 'address_line1-input',
-            'address_line2' => 'address_line2-input',
-            'address_line3' => 'address_line3-input',
-            'city' => 'city-input',
-            'company_number' => 'company_number-input',
-            'description' => 'description-input',
-            'directors' => 'directors-input',
-            'links' => 'links-input',
-            'name' => 'name-input',
-            'postal_code' => 'postal_code-input',
-          }],
-          meta: {
-            limit: 2,
-            cursors: {},
-          },
-        }.to_json,
-        headers: response_headers
-      )
-    end
-
-    it 'automatically makes the extra requests' do
-      expect(client.verification_details.all.to_a.length).to eq(2)
-      expect(first_response_stub).to have_been_requested
-      expect(second_response_stub).to have_been_requested
-    end
-  end
-
   describe '#create' do
     subject(:post_create_response) { client.verification_details.create(params: new_resource) }
     context 'with a valid request' do
@@ -266,6 +144,128 @@ describe GoCardlessPro::Resources::VerificationDetail do
         expect { post_create_response }.to raise_error(GoCardlessPro::InvalidStateError)
         expect(post_stub).to have_been_requested
       end
+    end
+  end
+
+  describe '#list' do
+    describe 'with no filters' do
+      subject(:get_list_response) { client.verification_details.list }
+
+      before do
+        stub_request(:get, %r{.*api.gocardless.com/verification_details}).to_return(
+          body: {
+            'verification_details' => [{
+
+              'address_line1' => 'address_line1-input',
+              'address_line2' => 'address_line2-input',
+              'address_line3' => 'address_line3-input',
+              'city' => 'city-input',
+              'company_number' => 'company_number-input',
+              'description' => 'description-input',
+              'directors' => 'directors-input',
+              'links' => 'links-input',
+              'name' => 'name-input',
+              'postal_code' => 'postal_code-input',
+            }],
+            meta: {
+              cursors: {
+                before: nil,
+                after: 'ABC123',
+              },
+            },
+          }.to_json,
+          headers: response_headers
+        )
+      end
+
+      it 'wraps each item in the resource class' do
+        expect(get_list_response.records.map do |x|
+                 x.class
+               end.uniq.first).to eq(GoCardlessPro::Resources::VerificationDetail)
+
+        expect(get_list_response.records.first.address_line1).to eq('address_line1-input')
+
+        expect(get_list_response.records.first.address_line2).to eq('address_line2-input')
+
+        expect(get_list_response.records.first.address_line3).to eq('address_line3-input')
+
+        expect(get_list_response.records.first.city).to eq('city-input')
+
+        expect(get_list_response.records.first.company_number).to eq('company_number-input')
+
+        expect(get_list_response.records.first.description).to eq('description-input')
+
+        expect(get_list_response.records.first.directors).to eq('directors-input')
+
+        expect(get_list_response.records.first.name).to eq('name-input')
+
+        expect(get_list_response.records.first.postal_code).to eq('postal_code-input')
+      end
+
+      it 'exposes the cursors for before and after' do
+        expect(get_list_response.before).to eq(nil)
+        expect(get_list_response.after).to eq('ABC123')
+      end
+
+      specify { expect(get_list_response.api_response.headers).to eql('content-type' => 'application/json') }
+    end
+  end
+
+  describe '#all' do
+    let!(:first_response_stub) do
+      stub_request(:get, %r{.*api.gocardless.com/verification_details$}).to_return(
+        body: {
+          'verification_details' => [{
+
+            'address_line1' => 'address_line1-input',
+            'address_line2' => 'address_line2-input',
+            'address_line3' => 'address_line3-input',
+            'city' => 'city-input',
+            'company_number' => 'company_number-input',
+            'description' => 'description-input',
+            'directors' => 'directors-input',
+            'links' => 'links-input',
+            'name' => 'name-input',
+            'postal_code' => 'postal_code-input',
+          }],
+          meta: {
+            cursors: { after: 'AB345' },
+            limit: 1,
+          },
+        }.to_json,
+        headers: response_headers
+      )
+    end
+
+    let!(:second_response_stub) do
+      stub_request(:get, %r{.*api.gocardless.com/verification_details\?after=AB345}).to_return(
+        body: {
+          'verification_details' => [{
+
+            'address_line1' => 'address_line1-input',
+            'address_line2' => 'address_line2-input',
+            'address_line3' => 'address_line3-input',
+            'city' => 'city-input',
+            'company_number' => 'company_number-input',
+            'description' => 'description-input',
+            'directors' => 'directors-input',
+            'links' => 'links-input',
+            'name' => 'name-input',
+            'postal_code' => 'postal_code-input',
+          }],
+          meta: {
+            limit: 2,
+            cursors: {},
+          },
+        }.to_json,
+        headers: response_headers
+      )
+    end
+
+    it 'automatically makes the extra requests' do
+      expect(client.verification_details.all.to_a.length).to eq(2)
+      expect(first_response_stub).to have_been_requested
+      expect(second_response_stub).to have_been_requested
     end
   end
 end
