1df810f27f2d1b5b1d04e1d478b29995617543e4
Robot <robot@gocardless.com>
2023-03-09 15:06:55 +0000

    Changes from gocardless/gocardless-pro-ruby-template@4ac7e91e493a5ae73a07f6693ff514a9ff247c0f

--- lib/gocardless_pro/resources/redirect_flow.rb
+++ lib/gocardless_pro/resources/redirect_flow.rb
@@ -10,6 +10,12 @@ module GoCardlessPro
   module Resources
     # Represents an instance of a redirect_flow resource returned from the API
 
+    # <p class="deprecated-notice"><strong>Deprecated</strong>: Redirect Flows
+    # are our legacy APIs for setting up
+    # mandates and will no longer be supported in the future. We strongly
+    # recommend using the
+    # [Billing Request flow](#billing-requests) instead.</p>
+    #
     # Redirect flows enable you to use GoCardless' [hosted payment
     # pages](https://pay-sandbox.gocardless.com/AL000000AKFPFF) to set up
     # mandates with your customers. These pages are fully compliant and have
