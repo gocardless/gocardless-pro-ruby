23fabab668448368d4ceb01b8a0ff819e2c191dc
Robot <robot@gocardless.com>
2025-05-19 13:05:21 +0000

    Changes from gocardless/gocardless-pro-ruby-template@153ea3c74b7f68c013a4a608dc87b9b1b6cf8826

--- lib/gocardless_pro/client.rb
+++ lib/gocardless_pro/client.rb
@@ -253,7 +253,7 @@ module GoCardlessPro
           'User-Agent' => "#{user_agent}",
           'Content-Type' => 'application/json',
           'GoCardless-Client-Library' => 'gocardless-pro-ruby',
-          'GoCardless-Client-Version' => '3.7.0'
+          'GoCardless-Client-Version' => '3.8.0'
         }
       }
     end
--- lib/gocardless_pro/version.rb
+++ lib/gocardless_pro/version.rb
@@ -3,5 +3,5 @@ end
 
 module GoCardlessPro
   # Current version of the GC gem
-  VERSION = '3.7.0'
+  VERSION = '3.8.0'
 end
