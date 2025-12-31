1bafcd7b47ba7cd0d78e3af802665232c812db83
Anya Mankani <amankani@gocardless.com>
2025-12-31 16:03:56 +0000

    Revert "Use hashrocket and test error message wrapping"
    This reverts commit b646cff8b3db4c9c35d8e8cd59de5c6da8beb1fc.

--- lib/gocardless_pro/middlewares/raise_gocardless_errors.rb
+++ lib/gocardless_pro/middlewares/raise_gocardless_errors.rb
@@ -13,19 +13,9 @@ module GoCardlessPro
         return unless CLIENT_ERROR_STATUSES.include?(env.status)
 
         json_body ||= JSON.parse(env.body) unless env.body.empty?
-
-        if json_body['error'].is_a?(String)
-          # Some errors are returned as string rather than objects, wrap in standard format
-          json_body['error'] = {
-            'message' => json_body['error'],
-            'status' => env.status,
-          }
-        end
-
         error_type = json_body['error']['type']
 
-        error_class = error_class_for_status(env.status) ||
-                      error_class_for_type(error_type)
+        error_class = error_class_for_status(env.status) || error_class_for_type(error_type)
 
         raise(error_class, json_body['error'])
       end
@@ -34,10 +24,9 @@ module GoCardlessPro
 
       def error_class_for_status(code)
         {
-          400 => GoCardlessPro::InvalidApiUsageError,
           401 => GoCardlessPro::AuthenticationError,
           403 => GoCardlessPro::PermissionError,
-          429 => GoCardlessPro::RateLimitError,
+          429 => GoCardlessPro::RateLimitError
         }.fetch(code, nil)
       end
 
@@ -46,7 +35,7 @@ module GoCardlessPro
           validation_failed: GoCardlessPro::ValidationError,
           gocardless: GoCardlessPro::GoCardlessError,
           invalid_api_usage: GoCardlessPro::InvalidApiUsageError,
-          invalid_state: GoCardlessPro::InvalidStateError,
+          invalid_state: GoCardlessPro::InvalidStateError
         }.fetch(type.to_sym)
       end
 
@@ -56,7 +45,7 @@ module GoCardlessPro
                        "code: #{env.status}\n" \
                        "headers: #{env.response_headers}\n" \
                        "body: #{env.body}",
-          'code' => env.status,
+          'code' => env.status
         }
       end
 
